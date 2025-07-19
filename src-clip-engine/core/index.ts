c84a8af6e136b5f25661d99d31903ba5e83ffc57
linjingxiong <linjingxiong@orbbec.com>
2025-07-19 22:34:38 +0800

    删除 src-clip-engine/core

--- src-clip-engine/core/index.ts
+++
@@ -1,9 +0,0 @@
-import { ClipEngine } from './Engine';
-import { FilterNode, FilterPipeline, TrackRenderer } from './renderer';
-
-export{
-    ClipEngine,
-    FilterNode,
-    FilterPipeline,
-    TrackRenderer
-}
--- src-clip-engine/core/renderer/index.ts
+++
@@ -1,5 +0,0 @@
-import { FilterNode } from './FilterNode';
-import { FilterPipeline } from './FilterPipeline';
-import { TrackRenderer } from './TrackRenderer';
-
-export { FilterNode, FilterPipeline, TrackRenderer };
